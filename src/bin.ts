fff180ee6140da06cfe70d687d6684ad2e3cd4ff
Miles Johnson <mileswjohnson@gmail.com>
2020-10-15 18:46:51 -0700

    new: Check if package is outdated.

--- src/bin.ts
+++ src/bin.ts
@@ -1,17 +1,18 @@
-import { Program } from '@boost/cli';
+import { Program, checkPackageOutdated } from '@boost/cli';
 // eslint-disable-next-line unicorn/import-index
 import { BuildCommand } from './index';
+
+const version = String(require('../package.json').version);
 
 async function run() {
   const program = new Program({
     bin: 'packemon',
     footer: 'Documentation: https://packemon.dev',
     name: 'Packemon',
-    // eslint-disable-next-line
-    version: require('../package.json').version,
+    version,
   });
 
-  program.register(new BuildCommand());
+  program.middleware(checkPackageOutdated('packemon', version)).register(new BuildCommand());
 
   await program.runAndExit(process.argv);
 }
