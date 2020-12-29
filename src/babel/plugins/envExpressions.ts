c09880ad4956d1c4dcc760d02e0553fdeafb3ac7
Miles Johnson <mileswjohnson@gmail.com>
2020-12-28 18:07:59 -0800

    fix: Fix env expressions when used as an object property value.

--- src/babel/plugins/envExpressions.ts
+++ src/babel/plugins/envExpressions.ts
@@ -12,39 +12,36 @@ export default function envExpressions() {
       Identifier: {
         enter(path: NodePath<t.Identifier>) {
           Object.entries(exprs).forEach(([expr, [op, env]]) => {
-            if (
-              path.isIdentifier({ name: expr }) &&
-              // const __DEV__ = var;
-              !path.parentPath.isVariableDeclarator() &&
-              // { __DEV__: var }
-              !path.parentPath.isObjectProperty() &&
-              // obj.__DEV__ = var;
-              !path.parentPath.isMemberExpression()
-              // // if (__DEV__)
-              // (path.parentPath.isIfStatement() ||
-              //   // switch(__DEV__)
-              //   path.parentPath.isSwitchStatement() ||
-              //   // __DEV__ && var
-              //   path.parentPath.isLogicalExpression() ||
-              //   // __DEV__ ? a : b
-              //   path.parentPath.isConditionalExpression() ||
-              //   // !__DEV__
-              //   path.parentPath.isUnaryExpression() ||
-              //   // prop={__DEV__}
-              //   path.parentPath.isJSXExpressionContainer())
-            ) {
-              path.replaceWith(
-                t.binaryExpression(
-                  op as '===',
-                  t.memberExpression(
-                    t.memberExpression(t.identifier('process'), t.identifier('env'), false),
-                    t.identifier('NODE_ENV'),
-                    false,
-                  ),
-                  t.stringLiteral(env),
-                ),
-              );
+            if (!path.isIdentifier({ name: expr })) {
+              return;
             }
+
+            // const __DEV__ = var;
+            if (path.parentPath.isVariableDeclarator()) {
+              return;
+            }
+
+            // { __DEV__: var }, { [__DEV__]: var }
+            if (path.parentPath.isObjectProperty() && path.parentPath.node.value !== path.node) {
+              return;
+            }
+
+            // obj.__DEV__ = var;
+            if (path.parentPath.isMemberExpression()) {
+              return;
+            }
+
+            path.replaceWith(
+              t.binaryExpression(
+                op as '===',
+                t.memberExpression(
+                  t.memberExpression(t.identifier('process'), t.identifier('env'), false),
+                  t.identifier('NODE_ENV'),
+                  false,
+                ),
+                t.stringLiteral(env),
+              ),
+            );
           });
         },
       },
