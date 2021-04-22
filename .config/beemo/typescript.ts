bba7931abfeb010f5e6f3a2f0f474f4e638b8026
Miles Johnson <mileswjohnson@gmail.com>
2021-04-22 16:10:23 -0700

    Set TS module.

--- .config/beemo/typescript.ts
+++ .config/beemo/typescript.ts
@@ -3,6 +3,8 @@ export default {
     // We dont want the TS build to conflict with Packemon's build,
     // so lets output the original TS build to another folder temporarily.
     outDir: 'build',
+    module: 'commonjs',
+    target: 'es2018',
     // This changes the structure of the DTS output folder, so avoid it.
     resolveJsonModule: false,
   },
