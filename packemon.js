6707b78d07518eb813b74e10e6e0ae025a933604
Miles Johnson <mileswjohnson@gmail.com>
2021-07-16 00:34:10 -0700

    Use build instead.

---
+++ packemon.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-underscore-dangle */
+
+global.__DEV__ = true;
+global.__PROD__ = false;
+
+try {
+	// Packemon built
+	require('./packages/packemon/lib/bin');
+} catch {
+	// Babel built (initial setup)
+	require('./packages/packemon/build/bin');
+}
