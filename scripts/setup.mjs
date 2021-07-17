c9a1a184ee67aa7a1c4840eff1cf663fdfb1ebf7
Miles Johnson <mileswjohnson@gmail.com>
2021-07-17 11:38:33 -0700

    Dont use TLA.

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -1,10 +1,14 @@
 import execa from 'execa';
 import glob from 'fast-glob';
 
-const pkgs = await glob('./packages/*', { onlyDirectories: true });
-
 // Running `beemo babel` as a package script through Yarn 2
 // seems to hang on Windows, so let's run things manually here...
-await Promise.all(
-	pkgs.map((pkg) => execa('yarn', ['beemo', 'babel', `${pkg}/src`, '--out-dir', `${pkg}/lib`])),
-);
+function setup() {
+	const pkgs = await glob('./packages/*', { onlyDirectories: true });
+
+	await Promise.all(
+		pkgs.map((pkg) => execa('yarn', ['beemo', 'babel', `${pkg}/src`, '--out-dir', `${pkg}/lib`])),
+	);
+}
+
+setup();
