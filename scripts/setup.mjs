bf5522a6b3333643a37c1bd960805f7d2e4399e7
Miles Johnson <milesj@users.noreply.github.com>
2021-07-17 11:47:52 -0700

    build: Migrate to Yarn 2. (#79)
    * Install yarn.
    
    * Update config.
    
    * Fix deps.
    
    * Fix setup.
    
    * Add interactive plugin.
    
    * Try escaping.
    
    * Be explicit.
    
    * Debug.
    
    * Try escaping.
    
    * Try setup script.
    
    * Dont use TLA.
    
    * Add await.

---
+++ scripts/setup.mjs
@@ -0,0 +1,14 @@
+import execa from 'execa';
+import glob from 'fast-glob';
+
+// Running `beemo babel` as a package script through Yarn 2
+// seems to hang on Windows, so let's run things manually here...
+async function setup() {
+	const pkgs = await glob('./packages/*', { onlyDirectories: true });
+
+	await Promise.all(
+		pkgs.map((pkg) => execa('yarn', ['beemo', 'babel', `${pkg}/src`, '--out-dir', `${pkg}/lib`])),
+	);
+}
+
+setup();
