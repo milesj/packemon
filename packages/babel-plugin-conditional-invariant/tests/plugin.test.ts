cdcf414ef3ac3fac2a1c391a4e7099f14abab200
Miles Johnson <mileswjohnson@gmail.com>
2021-07-16 19:21:59 -0700

    Add tests.

--- packages/babel-plugin-conditional-invariant/tests/plugin.test.ts
+++ packages/babel-plugin-conditional-invariant/tests/plugin.test.ts
@@ -18,4 +18,89 @@ async function transform(code: string, options?: TransformOptions): Promise<stri
 	return result?.code ?? '';
 }
 
-describe('conditionalInvariantPlugin()', () => {});
+describe('conditionalInvariantPlugin()', () => {
+	it(`will transform valid expressions`, async () => {
+		expect(
+			await transform(`
+invariant();
+
+invariant(false);
+
+while (true) {
+	invariant(true);
+}
+
+do {
+	invariant(value === false, 'Some message');
+} while (true);
+
+switch (value) {
+	case 1:
+		invariant(value === true, 'Some message');
+		break;
+
+	case 2: {
+		invariant(value === true, 'Some message');
+		break;
+	}
+}
+
+if (1) {
+	invariant(!value, 'Some message', 'another arg');
+} else if (2) {
+	invariant(!value, 'Some message', 123);
+} else {
+	invariant(!!value, 'Some message', true, 456);
+}
+`),
+		).toMatchSnapshot();
+	});
+
+	it(`will transform if wrapped in a non-matching conditional`, async () => {
+		expect(
+			await transform(`
+if (process.env.NODE_ENV === 'development') {
+	invariant();
+}
+`),
+		).toMatchSnapshot();
+	});
+
+	it(`will transform if wrapped in a non-matching conditional that is layers deep`, async () => {
+		expect(
+			await transform(`
+if (process.env.NODE_ENV === 'development') {
+	if (true) {
+		if (false) {
+			invariant();
+		}
+	}
+}
+`),
+		).toMatchSnapshot();
+	});
+
+	it(`will not transform if wrapped in a matching conditional`, async () => {
+		expect(
+			await transform(`
+if (process.env.NODE_ENV !== 'production') {
+	invariant();
+}
+`),
+		).toMatchSnapshot();
+	});
+
+	it(`will not transform if wrapped in a matching conditional that is layers deep`, async () => {
+		expect(
+			await transform(`
+if (process.env.NODE_ENV !== 'production') {
+	if (true) {
+		if (false) {
+			invariant();
+		}
+	}
+}
+`),
+		).toMatchSnapshot();
+	});
+});
