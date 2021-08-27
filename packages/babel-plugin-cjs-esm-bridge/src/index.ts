9e130f4e9af2257d84761928072654d4b61015c2
Miles Johnson <mileswjohnson@gmail.com>
2021-08-26 18:27:44 -0700

    Test cjs exports.

--- packages/babel-plugin-cjs-esm-bridge/src/index.ts
+++ packages/babel-plugin-cjs-esm-bridge/src/index.ts
@@ -8,20 +8,38 @@ function isEsmFile(file: string) {
 export default function cjsEsmBridge(): PluginObj {
 	return {
 		visitor: {
-			CallExpression: {
-				enter(path: NodePath<t.CallExpression>, state) {
-					const isEsm = isEsmFile(state.filename);
+			CallExpression(path: NodePath<t.CallExpression>, state) {
+				const isEsm = isEsmFile(state.filename);
 
-					// require() not allowed in esm files
-					// https://nodejs.org/api/esm.html#esm_no_require_exports_or_module_exports
-					if (isEsm && path.get('callee').isIdentifier({ name: 'require' })) {
-						throw new Error(
-							`Found a \`require()\` call in "${paths.basename(
-								state.filename,
-							)}", this is not allowed in ".mjs" files. Use dynamic \`import()\` instead.`,
-						);
-					}
-				},
+				// `require()` not allowed in esm files
+				// https://nodejs.org/api/esm.html#esm_no_require_exports_or_module_exports
+				if (isEsm && path.get('callee').isIdentifier({ name: 'require' })) {
+					throw new Error(
+						`Found a \`require()\` call in non-module file "${paths.basename(
+							state.filename,
+						)}". Use dynamic \`import()\` instead.`,
+					);
+				}
+			},
+
+			MemberExpression(path: NodePath<t.MemberExpression>, state) {
+				const isEsm = isEsmFile(state.filename);
+
+				// `exports.name` not allowed in esm files
+				// https://nodejs.org/api/esm.html#esm_no_require_exports_or_module_exports
+				if (
+					isEsm &&
+					path.get('object').isIdentifier({ name: 'exports' }) &&
+					path.get('property').isIdentifier()
+				) {
+					const { name } = path.node.property as t.Identifier;
+
+					throw new Error(
+						`Found an \`exports.${name} =\` expression in non-module file "${paths.basename(
+							state.filename,
+						)}". Use \`export const ${name} =\` instead.`,
+					);
+				}
 			},
 		},
 	};
--- packages/babel-plugin-cjs-esm-bridge/tests/plugin.test.ts
+++ packages/babel-plugin-cjs-esm-bridge/tests/plugin.test.ts
@@ -22,28 +22,62 @@ describe('cjsEsmBridge()', () => {
 	describe('require()', () => {
 		it('errors if in a .ts file', async () => {
 			await expect(transform("require('foo');", { filename: 'file.ts' })).rejects.toThrow(
-				'Found a `require()` call in "file.ts", this is not allowed in ".mjs" files. Use dynamic `import()` instead.',
+				'Found a `require()` call in non-module file "file.ts". Use dynamic `import()` instead.',
 			);
 		});
 
 		it('errors if in a .tsx file', async () => {
 			await expect(transform("require('foo');", { filename: 'file.tsx' })).rejects.toThrow(
-				'Found a `require()` call in "file.tsx", this is not allowed in ".mjs" files. Use dynamic `import()` instead.',
+				'Found a `require()` call in non-module file "file.tsx". Use dynamic `import()` instead.',
 			);
 		});
 
 		it('errors if in a .mjs file', async () => {
 			await expect(transform("require('foo');", { filename: 'file.mjs' })).rejects.toThrow(
-				'Found a `require()` call in "file.mjs", this is not allowed in ".mjs" files. Use dynamic `import()` instead.',
+				'Found a `require()` call in non-module file "file.mjs". Use dynamic `import()` instead.',
 			);
 		});
 
 		it('doesnt error if in a .js file', async () => {
-			await expect(transform("require('foo');", { filename: 'file.js' })).resolves.toBeDefined();
+			await expect(transform("require('foo');", { filename: 'file.js' })).resolves.not.toThrow();
 		});
 
 		it('doesnt error if in a .cjs file', async () => {
-			await expect(transform("require('foo');", { filename: 'file.cjs' })).resolves.toBeDefined();
+			await expect(transform("require('foo');", { filename: 'file.cjs' })).resolves.not.toThrow();
+		});
+	});
+
+	describe('exports.<name>', () => {
+		it('errors if in a .ts file', async () => {
+			await expect(transform('exports.foo = 123;', { filename: 'file.ts' })).rejects.toThrow(
+				'Found an `exports.foo =` expression in non-module file "file.ts". Use `export const foo =` instead.',
+			);
+		});
+
+		it('errors if in a .tsx file', async () => {
+			await expect(transform("exports.bar = 'abc';", { filename: 'file.tsx' })).rejects.toThrow(
+				'Found an `exports.bar =` expression in non-module file "file.tsx". Use `export const bar =` instead.',
+			);
+		});
+
+		it('errors if in a .mjs file', async () => {
+			await expect(transform('exports.baz = true', { filename: 'file.mjs' })).rejects.toThrow(
+				'Found an `exports.baz =` expression in non-module file "file.mjs". Use `export const baz =` instead.',
+			);
+		});
+
+		it('doesnt error if in a .js file', async () => {
+			await expect(transform('exports.foo = 123;', { filename: 'file.js' })).resolves.not.toThrow();
+		});
+
+		it('doesnt error if in a .cjs file', async () => {
+			await expect(
+				transform('exports.baz = true', { filename: 'file.cjs' }),
+			).resolves.not.toThrow();
+		});
+
+		it('doesnt error for other member expressions', async () => {
+			await expect(transform('obj.prop = 123', { filename: 'file.mjs' })).resolves.not.toThrow();
 		});
 	});
 });
