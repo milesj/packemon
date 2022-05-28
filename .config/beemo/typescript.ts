19e1fc6b39515475248a7c8939ca50c3b4deba49
Miles Johnson <mileswjohnson@gmail.com>
2022-05-27 20:27:03 -0700

    Add patches.

--- .config/beemo/typescript.ts
+++ .config/beemo/typescript.ts
@@ -1,6 +1,7 @@
 export default {
 	compilerOptions: {
 		jsx: 'react-jsx',
+		moduleResolution: 'nodenext',
 		outDir: 'build',
 		// This changes the structure of the DTS output folder, so avoid it.
 		resolveJsonModule: false,
