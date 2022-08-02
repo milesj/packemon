e851f0b3bd3aa832015b73e01a8eeb39b5ffa60b
Miles Johnson <mileswjohnson@gmail.com>
2022-08-02 16:20:05 -0700

    Use default conditions.

--- scenarios/jest/jest.config.js
+++ scenarios/jest/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	preset: 'jest-preset-beemo',
 	testEnvironment: 'jsdom',
 	testEnvironmentOptions: {
-		customExportConditions: ['browser', 'default'],
+		customExportConditions: ['browser'],
 	},
 };
