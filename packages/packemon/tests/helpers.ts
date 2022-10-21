6a8c71be46567314ee9a95105ce322c8d8dbd75c
Miles Johnson <mileswjohnson@gmail.com>
2022-10-21 13:37:56 -0700

    Move examples.

--- packages/packemon/tests/helpers.ts
+++ packages/packemon/tests/helpers.ts
@@ -1,8 +1,46 @@
 import fs from 'fs';
 import fsx from 'fs-extra';
 import { Path, PortablePath, toArray } from '@boost/common';
-import { Package } from '../src/Package';
-import { PackemonPackage } from '../src/types';
+import { getFixturePath } from '@boost/test-utils';
+import {
+	Artifact,
+	Format,
+	FORMATS,
+	FORMATS_BROWSER,
+	FORMATS_NATIVE,
+	FORMATS_NODE,
+	Package,
+	PackageConfig,
+	PackemonPackage,
+	Platform,
+	PLATFORMS,
+	Support,
+	SUPPORTS,
+} from '../src';
+
+const builds = new Map<string, { format: Format; platform: Platform; support: Support }>();
+
+FORMATS.forEach((format) => {
+	PLATFORMS.forEach((platform) => {
+		SUPPORTS.forEach((support) => {
+			const key = `${format}:${platform}:${support}`;
+
+			if (
+				(platform === 'browser' && !(FORMATS_BROWSER as string[]).includes(format)) ||
+				(platform === 'native' && !(FORMATS_NATIVE as string[]).includes(format)) ||
+				(platform === 'node' && !(FORMATS_NODE as string[]).includes(format))
+			) {
+				return;
+			}
+
+			builds.set(key, {
+				format,
+				platform,
+				support,
+			});
+		});
+	});
+});
 
 export function mockSpy(instance: unknown): jest.SpyInstance {
 	return instance as jest.SpyInstance;
@@ -88,4 +126,59 @@ export function createSnapshotSpies(root: PortablePath, captureJson: boolean = f
 
 		return snapshots.sort((a, b) => a[0].localeCompare(b[0]));
 	};
+}
+
+export function testExampleOutput(
+	file: string,
+	transformer: 'babel' | 'swc',
+	options?: Partial<PackageConfig>,
+	customRoot?: Path,
+) {
+	// eslint-disable-next-line jest/valid-title
+	describe(transformer, () => {
+		const root = customRoot ?? getFixturePath('examples');
+		const snapshots = createSnapshotSpies(root);
+
+		beforeEach(() => {
+			process.env.PACKEMON_TEST_WRITE = 'true';
+
+			if (transformer === 'swc') {
+				process.env.PACKEMON_SWC = 'true';
+			}
+		});
+
+		afterEach(() => {
+			delete process.env.PACKEMON_TEST_WRITE;
+			delete process.env.PACKEMON_SWC;
+		});
+
+		[...builds.values()].forEach((build) => {
+			const pkg = loadPackageAtPath(root);
+			const env = `${build.platform}-${build.support}-${build.format}`;
+
+			it(`transforms example test case: ${env}`, async () => {
+				const artifact = new Artifact(pkg, [build]);
+				artifact.platform = build.platform;
+				artifact.support = build.support;
+				artifact.inputs = { [`index-${env}`]: file };
+
+				// eslint-disable-next-line jest/no-conditional-in-test
+				if (options) {
+					Object.assign(artifact, options);
+				}
+
+				pkg.artifacts.push(artifact);
+
+				try {
+					await pkg.build({}, {});
+				} catch (error: unknown) {
+					console.error(error);
+				}
+
+				snapshots().forEach((ss) => {
+					expect(ss).toMatchSnapshot();
+				});
+			});
+		});
+	});
 }
