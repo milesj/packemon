6bab2b06bbb0bfd4fecc302f76012ca200b36203
Miles Johnson <mileswjohnson@gmail.com>
2022-10-26 15:01:42 -0700

    Start on add engines.

--- packages/packemon/src/Package.ts
+++ packages/packemon/src/Package.ts
@@ -16,6 +16,8 @@ import {
 	FORMATS_BROWSER,
 	FORMATS_NATIVE,
 	FORMATS_NODE,
+	NODE_SUPPORTED_VERSIONS,
+	SUPPORT_PRIORITY,
 } from './constants';
 import { loadTsconfigJson } from './helpers/loadTsconfigJson';
 import { matchesPattern } from './helpers/matchesPattern';
@@ -91,7 +93,7 @@ export class Package {
 
 		// Add package `engines` based on artifacts
 		if (options.addEngines) {
-			// this.addEngines();
+			this.addEngines();
 		}
 
 		// Add package `exports` based on artifacts
@@ -379,6 +381,32 @@ export class Package {
 
 	async syncJson() {
 		await fs.writeJson(this.jsonPath.path(), this.json, { spaces: 2 });
+	}
+
+	protected addEngines() {
+		const artifact = this.artifacts
+			.filter((art) => art.platform === 'node')
+			.reduce<Artifact | null>(
+				(oldest, art) =>
+					!oldest || SUPPORT_PRIORITY[art.support] < SUPPORT_PRIORITY[oldest.support]
+						? art
+						: oldest,
+				null,
+			);
+
+		if (!artifact) {
+			return;
+		}
+
+		this.debug('Adding `engines` to `package.json`');
+
+		if (!this.json.engines) {
+			this.json.engines = {};
+		}
+
+		Object.assign(this.json.engines, {
+			node: `>=${NODE_SUPPORTED_VERSIONS[artifact.support]}`,
+		});
 	}
 
 	protected addFiles() {
