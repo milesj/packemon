d0bf42f7667d6a42f049e565b208deaf7160552d
Miles Johnson <mileswjohnson@gmail.com>
2022-10-28 14:06:20 -0700

    Try some babel stuff.

--- scripts/buildPackages.mjs
+++ scripts/buildPackages.mjs
@@ -1,10 +1,32 @@
 import execa from 'execa';
+import path from 'path';
 
 async function build(cwd, format = 'lib') {
-	await execa('babel', ['src/', '--out-dir', format], { cwd, preferLocal: true });
+	await execa(
+		'babel',
+		[
+			'src',
+			'--out-dir',
+			format,
+			'--extensions',
+			'.ts,.tsx',
+			'--out-file-extension',
+			format === 'lib' ? '.js' : '.cjs',
+			// We can't use the root `babel.config.js` as the moon preset
+			// uses packages from this repo, so it never resolves.
+			'--config-file',
+			path.join(process.cwd(), 'scripts/babel.config.js'),
+		],
+		{
+			cwd: path.join(process.cwd(), cwd),
+			// env: { DEBUG: 'babel:*', NO_INTEROP: 'true' },
+			preferLocal: true,
+			stdio: 'inherit',
+		},
+	);
 }
 
 await build('packages/babel-plugin-cjs-esm-interop');
-await build('packages/babel-plugin-conditional-variant');
+await build('packages/babel-plugin-conditional-invariant');
 await build('packages/babel-plugin-env-constants');
 await build('packages/packemon', 'cjs');
