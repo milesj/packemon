097ac2052f381678906c8b6c1b503377301cb862
Miles Johnson <mileswjohnson@gmail.com>
2024-02-29 21:38:32 -0800

    Fix css external.

--- tests/__fixtures__/examples/asset-imports.ts
+++ tests/__fixtures__/examples/asset-imports.ts
@@ -3,6 +3,6 @@ import './styles.css';
 import './logo.svg';
 
 // Verify assets in node modules are not copied
-import 'website/src/css/custom.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 console.log(json);
