13e464e045f145d280b2cf917ac0650f9b34777d
Miles Johnson <mileswjohnson@gmail.com>
2024-03-09 15:49:52 -0800

    fix: Fix the wrong version being displayed.

--- packages/packemon/src/helpers/getVersion.ts
+++ packages/packemon/src/helpers/getVersion.ts
@@ -1,6 +1,10 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { PackageStructure } from '@boost/common';
 import { FileSystem } from '../FileSystem';
 
+const PKG_PATH = path.join(path.dirname(fileURLToPath(import.meta.url)), '../../package.json');
+
 export function getVersion(fs: FileSystem): string {
-	return fs.readJson<PackageStructure>(new URL('../../package.json', import.meta.url).href).version;
+	return fs.readJson<PackageStructure>(PKG_PATH).version;
 }
